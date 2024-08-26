5e985e345b36f1731afd7d5834502a84ecdb72a7
Dinil_Denny <dinildenny99@gmail.com>
2024-08-26 11:15:10 +0530

    function component - component composition

--- app.js
+++ app.js
@@ -1,3 +1,14 @@
-const heading = React.createElement("h1",{id:"heading"},"Welcome");
+import ReactDOM from 'react-dom/client';
+const Description = () => (
+    <p id='description'>This tutorial teach React</p>
+);
+const Heading = () => (
+    <div>
+        <h1 id='heading'>Hello React</h1>
+        <Description/>
+    </div>
+    
+);
+
 const root = ReactDOM.createRoot(document.getElementById('container'));
-root.render(heading);
+root.render(<Heading/>);
